565a26cc07671a2deb5b81e9a9800bdb0ac6d4f7
alex-toy <alexei.80@hotmail.fr>
2024-07-06 19:10:30 +0200

    quiz

---
+++ section-8-1/quiz/src/components/Questions.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar"></div>
+            <div class="status">1 out of 3 questions answered</div>
+        </div>
+        <div class="single-question" 
+            v-for="(question, index) in questions" 
+            :key="question.q" 
+            v-show="questionsAnsweredCount === index">
+            <div class="question">{{ question.q }}</div>
+            <div class="answers">
+                <div class="answer" 
+                    v-for="answer in question.answers" 
+                    :key="answer"
+                    @click.prevent="selectAnswer">
+                    {{ answer.text }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : ['questions', 'questionsAnsweredCount'],
+    methods : {
+        selectAnswer() {
+            
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+</style>
+  
