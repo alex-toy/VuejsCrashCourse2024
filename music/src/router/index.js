be57d7dc89d143173a9faba45d3559c904a147bb
alex-toy <alexei.80@hotmail.fr>
2024-07-27 07:26:46 +0200

    routes

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
-    component: AboutView
+    component: About
   }
 ]
 
